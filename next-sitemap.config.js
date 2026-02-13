afff80c528b52e927ec80dba966da3b9ab4bc995
Mousa Abuzar <mousaabuzar@gmail.com>
2026-02-13 16:18:39 -0700

    Add sitemap and robots

---
+++ next-sitemap.config.js
@@ -0,0 +1,5 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: process.env.SITE_URL || "https://campus-coach-q1rf.vercel.app/sitemap.xml",
+  generateRobotsTxt: true,
+};
